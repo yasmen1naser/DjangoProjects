d685b12a78301cd68c4f5d742307802c60de68cb
yasmen <yasmin1naser@gmail.com>
2026-04-05 13:20:04 +0300

    آخر تحديث

--- libraryproject/apps/bookmodule/urls.py
+++ libraryproject/apps/bookmodule/urls.py
@@ -6,4 +6,10 @@ urlpatterns = [
     path('list_books/', views.list_books, name= "books.list_books"), 
     path('<int:bookId>/', views.viewbook, name="books.view_one_book"), 
     path('aboutus/', views.aboutus, name="books.aboutus"), 
+    path('html5/links/', views.links, name="books.links"),
+    path('html5/text/formatting/', views.formatting, name="books.formatting"),
+    path('html5/listing/', views.listing, name="books.listing"),
+    path('html5/tables/', views.tables, name="books.tables"),
+    path('search/', views.search, name="books.search"),
+
 ] 
--- libraryproject/apps/bookmodule/views.py
+++ libraryproject/apps/bookmodule/views.py
@@ -10,3 +10,21 @@ def viewbook(request, bookId):
     return render(request, 'bookmodule/one_book.html') 
 def aboutus(request): 
     return render(request, 'bookmodule/aboutus.html') 
+
+
+def links(request):
+    return render(request, "bookmodule/links.html")
+
+def formatting(request):
+    return render(request, "bookmodule/formatting.html")
+
+def listing(request):
+    return render(request, "bookmodule/listing.html")
+
+def tables(request):
+    return render(request, "bookmodule/tables.html")
+from django.shortcuts import render
+
+def search(request):
+    query = request.GET.get('!', '')  
+    return render(request, 'bookmodule/search_results.html', {'query': query})
